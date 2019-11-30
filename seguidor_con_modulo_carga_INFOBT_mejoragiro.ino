0c7678e4fbccd1ca084a3c3d1e1df7a92e49e9c2
leyvamn2 <56892926+leyvamn2@users.noreply.github.com>
2019-11-30 14:15:47 -0600

    Final line follower

---
+++ seguidor_con_modulo_carga_INFOBT_mejoragiro.ino
@@ -0,0 +1,290 @@
+/*
+    Proyecto para la materia de Desarrollo de proyectos de automatización
+
+
+    En esta versión se cambiaron varias cosas: Para hacer girar y dejar de girar se dividieron las funciones en dos estados,
+    en vez de tenerlas en 4 estados como antes para que solo existan 2 estados y no se combinen
+    El beep es intermitente cada medio segundo, no es continuo.
+    Se usa el módulo BT para poder leer la información desde el Putty mediante la conexión previa con la compu.
+    Sólo falta crear un temporizador, definir cuándo empieza y termina un recorrido y poner a funcionar el módulo de carga.
+*/
+#include "AFMotor.h"
+#define SensorUno A8
+#define SensorDos A9
+#define trig 22// cable azul
+#define echo 24// cable verde
+#define buzzer 40
+#define luces 30
+#define luces1 32
+#define PB 44
+
+/*Motores*/
+AF_DCMotor MotorUno(1);// derecha adelante
+AF_DCMotor MotorDos(2);// izquierda adelante
+AF_DCMotor MotorTres(3);// derecha atras
+AF_DCMotor MotorCuatro(4);// izquierda atras
+
+int duration, distance;
+byte LsensorU;
+byte LsensorD;
+float t, t1=0,t2=0;
+int  starte=0;
+int val,readd,k,kk;
+
+
+
+void setup() {
+  /* parte 1. sensores infrarrojos*/
+  pinMode(SensorUno, INPUT);
+  pinMode(SensorDos, INPUT);
+  /*para ver el estado del carrito en las pruebas*/
+  Serial3.begin(9600);
+  Serial.begin(9600);
+  /*Parte 2. Sensor ultrasónico*/
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  /*Parte 3. Buzzer*/
+  pinMode(buzzer,OUTPUT);
+  /*Parte 4. Módulo de carga*/
+  pinMode(PB,INPUT);
+  pinMode(luces,OUTPUT);
+  pinMode(luces1,OUTPUT);
+  Serial3.print("************BIENVENIDO AL REGISTRO DEL VEHÍCULO AUTOMATIZADO************\n En esta pantalla se le mostrarán parámetros de tiempo y estatus de los recorridos que el vehículo realice \n\n");
+  Serial3.print("************INSTRUCCIONES DE USO*************\n 1.para la carga y descarga de materiales el vehiculo se tiene que encontar en la estación con las guías blancas.\n ");
+  Serial3.print("2. Para iniciar un recorrido se debe de cargar primeramente la carga y después cambiar el color de la estación de carga \n"); 
+  Serial3.print("3. El vehículo se detendrá cuando ambos sensores detecten color blanco.");
+  Serial3.print("\n");
+  Serial3.print("\n");
+  Serial3.print("\n");
+  Serial3.print("\n");
+  Serial3.print("\n");
+  Serial3.print("\n");
+
+  
+} 
+
+void loop() {
+  
+val = digitalRead(PB);
+Serial.print(val);
+ LsensorU = digitalRead(SensorUno);//A5 Sensor derecha cable naranja
+ LsensorD = digitalRead(SensorDos);//A4 Sensor izquierda cable café
+ 
+ if((t>0)||((t>0)&&((LsensorU == LOW && LsensorD == LOW)||kk==0))){
+   Serial3.print("Se dejó de detectar peso \n");
+   Serial3.print("Tiempo total de recorrido: ");
+   Serial3.println(t);
+   Serial3.print("segundos");
+   Serial3.println("\n");
+   
+   t=0,t1=0,t2=0;
+   state3();  
+   kk=8;
+  }
+
+  
+
+  if(LsensorU != LOW && LsensorD != LOW){
+    kk=1;
+    }
+   if((LsensorU ==LOW && LsensorD == LOW)){
+      kk=0;
+      state3();
+      }
+  while((val==HIGH)&&(kk=1)){
+    readd=1;
+    starte=1;
+    LsensorU = digitalRead(SensorUno);//A5 Sensor derecha cable naranja
+    LsensorD = digitalRead(SensorDos);
+    if((LsensorU ==LOW && LsensorD == LOW)){
+      kk=3;
+      }
+    
+    Serial.print("Peso detectado");
+    Serial.print("Iniciando recorrido");
+    
+     val = digitalRead(PB); 
+      digitalWrite(trig, HIGH);
+      delay(100);
+      digitalWrite(trig, LOW); 
+      duration = pulseIn(echo, HIGH);
+      distance = duration / 59  ;
+      Serial.println(distance);
+  
+      if(distance>=10){
+        digitalWrite(buzzer,LOW);
+        digitalWrite(luces,LOW);
+        digitalWrite(luces1,LOW);
+        state1();
+        t1++;
+        Serial.print("Movimiento");
+         
+        Serial.println(t1/10);
+        }
+    
+     if(distance<=10){
+      state2();
+      state4();
+      t2++;
+      Serial.println(t2/10);
+     
+
+    }
+    t=((t1/10)+(t2/2));
+    Serial.print(" \n");
+    }  
+   
+   
+    
+    
+
+
+
+}
+ 
+  
+  /*if(starte=1){
+     val = digitalRead(PB); 
+  int readd;
+  if(val==HIGH){
+    readd=1;
+    }  
+  if(val==LOW){
+    readd=2;
+    }
+    digitalWrite(trig, HIGH);
+  delay(100);
+  digitalWrite(trig, LOW); 
+  duration = pulseIn(echo, HIGH);
+  distance = duration / 59  ;
+  
+  if(distance>=10){
+      digitalWrite(buzzer,LOW);
+      state1();
+      t1++;
+      Serial.print("Movimiento"); 
+      Serial.println(t1);
+     delay(1000);
+    }
+    
+  if(distance<=10){
+    state2();
+    t2++;
+     Serial.println(t2);
+    delay(1000);
+
+    }
+  t=t1+t2;*/
+  /*if(readd=2){
+    starte=0;
+    Serial.print("Peso retirado");
+    Serial.print("terminado recorrido");
+    Serial.print("tiempo total del recorrido\n");
+    Serial.println(t);
+    Serial.print(" S");
+    
+     }
+           
+   
+  }
+  
+    if(starte=0){
+      starte=3;
+      readd=3;
+    if(starte=3){
+    t=0;
+    t1=0;
+    t2=0;
+    }
+    }
+   */
+
+
+  
+
+ /*estados de movimiento*/
+  void state1(){
+  LsensorU = digitalRead(SensorUno);//A5 Sensor derecha cable naranja
+  LsensorD = digitalRead(SensorDos);//A4 Sensor izquierda cable café
+   if (LsensorU == HIGH && LsensorD == HIGH) {
+    MotorUno.setSpeed(650);
+    MotorDos.setSpeed(650);
+    MotorTres.setSpeed(650);
+    MotorCuatro.setSpeed(650);
+    MotorUno.run(FORWARD); //para que vaya hacia adelante
+    MotorDos.run(BACKWARD); // para que vaya hacia adelante
+    MotorTres.run(BACKWARD); // hacia delante
+    MotorCuatro.run(BACKWARD);// hacia delante
+    Serial3.println("avanza"); //esto es para ver el estado
+    }
+  if (LsensorU == HIGH && LsensorD == LOW) {
+    MotorUno.setSpeed(700);
+    MotorDos.setSpeed(700);
+    MotorTres.setSpeed(700);
+    MotorCuatro.setSpeed(700);
+    MotorUno.run(FORWARD); //hacia atrás llanta DAdelante
+    MotorDos.run(BACKWARD);//hacia delante llanta IAdelante
+    MotorTres.run(FORWARD); // hacia atrás DTrasera
+    MotorCuatro.run(BACKWARD);// hacia delante ITrasera
+    Serial3.println("IZQUIERDA");
+    }
+  if (LsensorU == LOW && LsensorD == HIGH) {
+   // para que se muva a la izquierda necesita detectar que el de la derecha esté en blanco
+    Serial3.println("DERECHA");
+    MotorUno.setSpeed(700);
+    MotorDos.setSpeed(700);
+    MotorTres.setSpeed(700);
+    MotorCuatro.setSpeed(700);
+    MotorUno.run(FORWARD); //hacia delante llanta DAdelante
+    MotorDos.run(BACKWARD);//hacia atrás llanta IAdelante
+    MotorTres.run(BACKWARD); // hacia delante DTrasera
+    MotorCuatro.run(FORWARD);// hacia atrás ITrasera  
+    }
+
+  }
+
+  void state2(){
+    
+     
+     
+        //  Cuando se detecte un objeto a 10 cm de distancia con el ultrasónico el carrito deberá de parar, además se debe de emitir una alarma
+    MotorUno.run(RELEASE);
+    MotorDos.run(RELEASE);
+    MotorTres.run(RELEASE);
+    MotorCuatro.run(RELEASE);
+
+    Serial.println("Objeto detectado");
+    
+    
+     }
+
+    void state3(){
+      for(int z=1;z<2;z++){
+      Serial3.println("ESPERA");
+      if(z=2){
+        z=8;}  
+        }
+      
+      MotorUno.run(RELEASE); 
+      MotorDos.run(RELEASE);//
+      MotorTres.run(RELEASE); 
+      MotorCuatro.run(RELEASE);   
+      digitalWrite(buzzer,LOW);
+       digitalWrite(luces,LOW);
+        digitalWrite(luces1,LOW);
+      }
+
+    void state4(){
+      for(int c=0; c<1;c++){
+      digitalWrite(buzzer,LOW);
+      digitalWrite(luces,LOW);
+      digitalWrite(luces1,LOW);
+      
+    }
+    for(int k=0; k<1;k++){
+      digitalWrite(buzzer,HIGH);
+      digitalWrite(luces,HIGH);
+      digitalWrite(luces1,HIGH);
+
+    }
+      }
